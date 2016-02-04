2f31040f588d3c1ccbd4731320349bce542dc9d0
Orlando Hohmeier <orlando@mesosphere.io>
2016-02-04 11:52:56 +0100

    Update main package entry

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-// main.js
+export PluginActions from "./plugin/PluginActions";
+export PluginDispatcher from "./plugin/PluginDispatcher";
+export PluginEvents from "./plugin/PluginEvents";
+export PluginHelper from "./plugin/PluginHelper";
+export PluginMountPoints from "./plugin/PluginMountPoints";
